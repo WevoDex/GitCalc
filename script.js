67b48b785323e4990419d19f8f60f6bc9f2afec6
kalikex1 <vidishashah20023@gmail.com>
2025-01-11 18:01:40 +0530

    Code Updated

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
-const element = document.getElementById("intro");
-console.log(element);
+let count = 0;
+const counter = (function () {
+    function clickOne() {
+        count++;
+        clickOne = document.getElementsByClassName("scoreText")[0].innerHTML = count;
+        console.log(count);
+    }
+    function clickTwo() {
+        count += 2;
+        clickTwo = document.getElementsByClassName("scoreText")[0].innerHTML = count;
+        console.log(count);
+    }
+
+    function clickThree() {
+        count += 3;
+        clickThree = document.getElementsByClassName("scoreText")[0].innerHTML = count;
+        console.log(count);
+    }
+
+    function guestOne() {
+        count++;
+        guestOne = document.getElementsByClassName("guestscore")[0].innerHTML = count;
+        console.log(count)
+    }
+    function guestTwo() {
+        count += 2;
+        guestTwo = document.getElementsByClassName("guestscore")[0].innerHTML = count;
+        console.log(count)
+    }
+    function guestThree() {
+        count += 3;
+        guestThree = document.getElementsByClassName("guestscore")[0].innerHTML = count;
+        console.log(count)
+    }
+    return {
+        clickOne,
+        clickTwo,
+        clickThree,
+        guestOne,
+        guestTwo,
+        guestThree
+    }
+})();
